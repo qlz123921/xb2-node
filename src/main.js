b82dceff20e78d5aa3a826630e0286650417bd20
qlz123921 <qiulinzong@gmail.com>
2024-09-07 17:44:54 +0800

    创建server

--- src/main.js
+++ src/main.js
@@ -1,2 +1,22 @@
-const greeting = '你好'
-console.log(greeting)
+const http = require('http')
+
+const server = http.createServer((require, response) => {
+  const data = {
+    id: 1,
+    title: '关山月',
+    content: '明月出天山,苍茫云海间',
+  }
+
+  const jsonData = JSON.stringify(data)
+  response.writeHead(200, {
+    'Content-type': 'application/json;charset=utf-8',
+  })
+
+  response.write(jsonData)
+
+  response.end()
+})
+
+server.listen(3002, () => {
+  console.log('🚀服务已启动!')
+})
