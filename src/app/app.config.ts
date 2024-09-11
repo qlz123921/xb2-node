2bcaf3c041a29acb877fa3cc9460ae0fa817ca84
qiulinzong <qiulinzong@gmail.com>
2024-09-11 20:21:58 +0800

    修改引入服务器的数据路径

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,14 +2,14 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-export const DB_HOST = 'localhost';
-export const DB_PORT = 5432;
+export const DB_HOST = process.env.DB_HOST || 'localhost';
+export const DB_PORT = parseInt(process.env.DB_PORT || '5432', 10);
 
-export const DB_USER = 'postgres';
+export const DB_USER = process.env.DB_USER || 'postgres';
 
-export const DB_PASSWORD = '#!2wsxzaq1';
+export const DB_PASSWORD = process.env.DB_PASSWORD || '';
 
-export const DB_NAME = 'xb2-node';
+export const DB_NAME = process.env.DB_NAME || 'xb2-node';
 /**
  * 应用配置
  */
