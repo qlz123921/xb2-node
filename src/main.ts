e93bb1b0419996e4e687018415bd6fbe3cb2819c
qiulinzong <qiulinzong@gmail.com>
2024-09-15 12:26:58 +0800

    链接服务器

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,30 @@
-import app from './app/index.js';
-import { APP_PORT } from './app/app.config.js';
+import app from './app/index.js'
+import { APP_PORT } from './app/app.config.js'
+import { PrismaClient } from '@prisma/client'
+import dayjs from 'dayjs'
 
-app.listen(APP_PORT, () => {
-  console.log('🚀服务已启动!!');
-});
+const prisma = new PrismaClient()
+
+app.listen(APP_PORT, async () => {
+  await prisma.$connect()
+
+  // const content = await prisma.content.create({
+  //   data: {
+  //     title: 'sdafs',
+  //     content: 'fasdfasdfasdf',
+  //     date_created: dayjs().format(),
+  //   },
+  // })
+
+  // console.log(content)
+
+  const contents = await prisma.content.findFirst({
+    where: {
+      id: 1,
+    },
+  })
+
+  console.log(contents)
+
+  console.log('🚀服务已启动!!')
+})
